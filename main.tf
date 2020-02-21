fde02fdc67705025736b888d8de779a5ab99893e
Michael Armstrong <michael@m80arm.co.uk>
2020-02-21 16:54:10 +0000

    Added Frankfurt

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
+  alias = "london"
   region     = var.region["london"]
 }
 
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
+  alias = "frankfurt" 
   region     = var.region["frankfurt"]
 }
 /*================
@@ -58,7 +60,7 @@ resource "aws_route_table_association" "vpc1_subnet2_routetable" {
 
 resource "aws_subnet" "vpc1_subnet3" {
   provider                = aws.london
-  vpc_id                  = aws_vpc.vpc1_ireland.id
+  vpc_id                  = aws_vpc.vpc1_london.id
   cidr_block              = var.my_london_subnets["vpc1_subnet3"]
   availability_zone       = "eu-west-2c"
   map_public_ip_on_launch = true
@@ -91,6 +93,92 @@ resource "aws_internet_gateway" "vpc1_IGW" {
 resource "aws_route" "vcp1_IGW_route" {
   provider               = aws.london
   route_table_id         = aws_vpc.vpc1_london.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc1_IGW.id
+}
+
+/*================
+Create MA-VPC-01 in Frankfurt
+=================*/
+resource "aws_vpc" "vpc1_frankfurt" {
+  provider             = aws.frankfurt
+  cidr_block           = var.my_frankfurt_subnets["vpc1"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "MA-VPC-01"
+  }
+}
+
+resource "aws_subnet" "vpc1_subnet1" {
+  provider                = aws.frankfurt
+  vpc_id                  = aws_vpc.vpc1_frankfurt.id
+  cidr_block              = var.my_frankfurt_subnets["vpc1_subnet1"]
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-01"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_subnet1_routetable" {
+  provider       = aws.frankfurt
+  subnet_id      = aws_subnet.vpc1_subnet1.id
+  route_table_id = aws_vpc.vpc1_frankfurt.default_route_table_id
+}
+
+resource "aws_subnet" "vpc1_subnet2" {
+  provider                = aws.frankfurt
+  vpc_id                  = aws_vpc.vpc1_london.id
+  cidr_block              = var.my_frankfurt_subnets["vpc1_subnet2"]
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-02"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_subnet2_routetable" {
+  provider       = aws.frankfurt
+  subnet_id      = aws_subnet.vpc1_subnet2.id
+  route_table_id = aws_vpc.vpc1_frankfurt.default_route_table_id
+}
+
+resource "aws_subnet" "vpc1_subnet3" {
+  provider                = aws.frankfurt
+  vpc_id                  = aws_vpc.vpc1_london.id
+  cidr_block              = var.my_frankfurtsubnets["vpc1_subnet3"]
+  availability_zone       = "eu-central-2c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-03"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_subnet3_routetable" {
+  provider       = aws.frankfurt
+  subnet_id      = aws_subnet.vpc1_subnet3.id
+  route_table_id = aws_vpc.vpc1_frankfurt.default_route_table_id
+}
+
+resource "aws_default_route_table" "vpc1_routetable" {
+  provider               = aws.frankfurt
+  default_route_table_id = aws_vpc.vpc1_frankfurt.default_route_table_id
+  tags = {
+    Name = "MA-VPC-01-DefaultRouteTable"
+  }
+}
+resource "aws_internet_gateway" "vpc1_IGW" {
+  provider = aws.frankfurt
+  vpc_id   = aws_vpc.vpc1_frankfurt.id
+  tags = {
+    Name = "MA-VPC-01-InternetGateway"
+  }
+}
+
+resource "aws_route" "vcp1_IGW_route" {
+  provider               = aws.frankfurt
+  route_table_id         = aws_vpc.vpc1_frankfurt.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.vpc1_IGW.id
 }
